a20d071eb6f20e1a00e3791844180bd68d818cc9
radotooo <radotooo@gmail.com>
2020-09-15 19:32:56 +0300

    tinder-clone front end

---
+++ Tinder-MERN/tinder-clone/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TinderCards from "./TinderCards";
+
+import "./App.css";
+import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <TinderCards />
+
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
