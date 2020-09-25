8e2db01f0a856d137c4ab7581e26856f40e62f08
radotooo <radotooo@gmail.com>
2020-09-25 17:56:27 +0300

    Modal done

--- RandomQuoteMashine/random-quote-mashine/src/components/Modal.js
+++ RandomQuoteMashine/random-quote-mashine/src/components/Modal.js
@@ -1,7 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useContext, useState } from 'react'
 import "./styles/modal.css"
 import TextField from '@material-ui/core/TextField';
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import MyContext from '../MyContext';
 
 const useStyle = makeStyles({
@@ -14,18 +15,59 @@ const useStyle = makeStyles({
 export default function Modal(props) {
 
     const { value, setValue } = useContext(MyContext)
+    const classes = useStyle();
+
+    const [quoteState, setQuoteState] = useState({
+        input: '',
+        error: false,
+        errorMsg: ""
+    })
+
+    const [authorState, setAuthorState] = useState({
+        input: '',
+        error: false,
+        errorMsg: ""
+    })
+
     const cssStyle = [
         "modal",
         value ? "fadeIn" : "fadeOut"
     ]
 
+    const handleSubmit = (e) => {
+        if (quoteState.input.trim().length === 0) {
+            setQuoteState((state) => ({
+                ...state,
+                error: true,
+                errorMsg: "Quote is Required!"
+            }));
+        }
+        if (authorState.input.trim().length === 0) {
+            setAuthorState((state) => ({
+                ...state,
+                error: true,
+                errorMsg: "Author is Required!"
+            }));
+        }
+    }
 
-    const classes = useStyle();
+    const resetState = () => {
+        setQuoteState((state) => ({
+            ...state,
+            error: false,
+            errorMsg: ""
+        }));
+        setAuthorState((state) => ({
+            ...state,
+            error: false,
+            errorMsg: ""
+        }));
+    }
 
     return (
         <div className={cssStyle.join(' ')}  >
             <div className="modal__container">
-                <form action="submit" id="form">
+                <form id="form" type="submit" onSubmit={(e) => (e.preventDefault())}>
                     <div>
                         <TextField
                             className={classes.root}
@@ -33,6 +75,9 @@ export default function Modal(props) {
                             label="Quote"
                             multiline
                             rows={3}
+                            error={quoteState.error}
+                            helperText={quoteState.errorMsg}
+                            onChange={(e) => { setQuoteState({ ...e, input: e.target.value }) }}
                             fullWidth
                             variant="outlined"
                             autoFocus
@@ -45,16 +90,19 @@ export default function Modal(props) {
                                 label="Author Name"
                                 margin="dense"
                                 variant="outlined"
+                                error={authorState.error}
+                                helperText={authorState.errorMsg}
+                                onChange={(e) => { setAuthorState({ ...e, input: e.target.value }) }}
                             />
                         </div>
                     </div>
                     <div className="modal__button_container">
-                        <a className="modal__button" style={{ backgroundColor: props.bkgColor }}>Submit</a>
-                        <a className="modal__button" style={{ backgroundColor: props.bkgColor }} onClick={(e) => { setValue(!value) }}> Cancel</a>
+                        <a className="modal__button" onClick={() => handleSubmit()} style={{ backgroundColor: props.bkgColor }}>Submit</a>
+                        <a className="modal__button" style={{ backgroundColor: props.bkgColor }} onClick={(e) => { setValue(!value); resetState(); }}> Cancel</a>
                     </div>
                 </form>
             </div>
-        </div>
+        </div >
     )
 }
 
