7aef9da8a63d7ad71d8f636ac713cf84d0975081
radotooo <radotooo@gmail.com>
2020-09-28 16:59:17 +0300

    app setup done

--- RandomQuoteMashine/random-quote-mashine-backEnd/app.js
+++ RandomQuoteMashine/random-quote-mashine-backEnd/app.js
@@ -1,12 +1,39 @@
-import express from "express";
-//const express = require('express')
+import express from "express"
+import Quote from "./models/Quote.js"
+import connectDB from "./config/db.js"
+
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+// Connect Database
+connectDB();
+
+//Middleware
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    const { title, author } = req.body
+    try {
+        const quote = new Quote({
+            title,
+            author
+        })
+        await quote.save();
+        res.send("user register")
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.get('/', async (req, res) => {
+    try {
+        var result = await Quote.aggregate([{ $sample: { size: 1 } }])
+        res.send(result)
+    } catch (error) {
+        res.send(error.message)
+    }
 })
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
-})
+}) 
